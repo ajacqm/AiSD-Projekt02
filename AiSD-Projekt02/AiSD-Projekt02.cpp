2992b320190101bd87568ce3385b18ea2594f57d
Jakub Matuszko <jakub@matj.me>
2021-01-09 13:13:29 +0100

    Ulepszenie funkcji main, przygotowanie funkcji dbl_vector

--- AiSD-Projekt02/AiSD-Projekt02.cpp
+++ AiSD-Projekt02/AiSD-Projekt02.cpp
@@ -21,36 +21,42 @@ using namespace std;
 
 bool show_op = true;		//Wyswietlanie wszystkich operacji w konsoli
 
-void insertion_sort();
-void bucket_sort();
+void insertion_sort(vector <int> &numb);
+void bucket_sort(vector <int> &numb);
+void dbl_vector(vector <int> numb,vector <int> &numb2);
 
 int main()
 {
-	vector <int> numbers;					//Deklaracja wektora liczb
-	ifstream input("input.txt");			//Deklaracja pliku wejściowego
-	ofstream output("output.txt");			//Deklaracja pliku wyjściowego
+	vector <int> numbers;								//Deklaracja wektora liczb
+	vector <int> numbers2;
+	vector <int> numbers3;
+	ifstream input("input.txt");						//Deklaracja pliku wejściowego
+	ofstream output("output.txt");						//Deklaracja pliku wyjściowego
 	
 	int f_read;
 
-	if (!input.good())						//Sprawdzenie poprawności otwarcia pliku:
-	{										//Jeżeli błąd:
-		cerr << "File error";				//Wyświetl komunikat o błędzie "File error"
-		return 1;							//Zwróc wartość 1
+	if (!input.good())									//Sprawdzenie poprawności otwarcia pliku:
+	{													//Jeżeli błąd:
+		cerr << "[ERROR] File error ";					//Wyświetl komunikat o błędzie "File error"
+		return 1;										//Zwróc wartość 1
 	}
-	else									//W przeciwnym wypadku (gdy program poprawnie odczytal plik):
+	else												//W przeciwnym wypadku (gdy program poprawnie odczytal plik):
 	{
-		while (!input.eof())				//Petla wczytujaca liczby z pliku do wektora
+		while (!input.eof())							//Petla wczytujaca liczby z pliku do wektora
 		{
-			input >> f_read;				//Przypisanie liczby z pliku do zmiennej f_read
-			if (input.fail())				//Jeżeli przypisanie zwróci błąd to:
+			input >> f_read;							//Przypisanie liczby z pliku do zmiennej f_read
+			if (input.fail())							//Jeżeli przypisanie zwróci błąd to:
 			{
-				input.clear();				//Usuniecie flagi bledu z wejscia
-				input.ignore();				//Zignorowanie znaku z pliku input.txt
+				if (show_op) cout << "[WARNING] Input - one character was ommited" << endl;
+
+				input.clear();							//Usuniecie flagi bledu z wejscia
+				input.ignore();							//Zignorowanie znaku z pliku input.txt
 			}							
-			else numbers.push_back(f_read);	//Gdy proces wskaże brak błędów, liczba zostanie dopisana do końca wektora
+			else numbers.push_back(f_read);				//Gdy proces wskaże brak błędów, liczba zostanie dopisana do końca wektora
 		}
 	}
-	if (show_op)									//Wyswietlenie wczytanej tablicy liczb do konsoli
+
+	if (show_op)										//Wyswietlenie wczytanej tablicy liczb do konsoli
 	{
 		cout << "Numbers from file: ";
 		for (int i = 0; i < numbers.size(); i++)
@@ -60,7 +66,21 @@ int main()
 		cout << endl;
 	}
 
+	dbl_vector(numbers, numbers2);						//Kopiowanie wektora numbers
+	dbl_vector(numbers, numbers3);						//Kopiowanie wektora numbers x 2
+
+
+
 	return 0;
+}
+
+void dbl_vector(vector <int> numb,vector <int> &numb2)
+{
+	for (int i = 0; i < numb.size(); i++)
+	{
+		numb2.push_back(numb[i]);
+	}
+	if (show_op) cout << "Vector sucessfully doubled" << endl;
 }
 
 void insertion_sort()
